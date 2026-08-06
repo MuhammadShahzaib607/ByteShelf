33ccf16d75ab4a4229a8764f5750fd90fb8c850d
MuhammadShahzaib607 <muhammadshahzaib6077@gmail.com>
2026-08-06 09:43:38 +0500

    deployment_7 (add frontend url in cors)

--- server/index.js
+++ server/index.js
@@ -48,6 +48,7 @@ initializeSocket(server)
 
 app.use(cors({
     origin: "http://localhost:3000",
+    origin: "https://byte-shelf-frontend.vercel.app/",
     credentials: true,
 }))
 app.use(express.json())
